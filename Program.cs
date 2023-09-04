c631205ddfef311f3c3046f178b6f1a14cd73a1a
Murat <murat.sadyrbekov@gmail.com>
2023-09-05 00:58:35 +0600

    add final code and exclude null Value

--- Program.cs
+++ Program.cs
@@ -8,20 +8,38 @@
 // лучше обойтись исключительно массивами.
 
 void FilterAndPrintShortStrings(string[] array)   // Метод принимает на вход массив строк и выводит строки <=3 символов
-    {
-        Console.WriteLine("Строки длиной 3 и меньше:");
+{
+    Console.WriteLine("Строки длиной 3 и меньше:");
 
-        foreach (string str in array)
+    foreach (string str in array)
+    {
+        if (str.Length <= 3)
         {
-            if (str.Length <= 3)
-            {
-                Console.WriteLine(str);
-            }
+            Console.WriteLine(str);
         }
     }
-
-
+}
 Console.Write("Введите размерность массива: ");
-int size = int.Parse(Console.ReadLine());
+int size;
+while (!int.TryParse(Console.ReadLine(), out size) || size <= 0)
+{
+    Console.WriteLine("Пожалуйста, введите положительное целое число для размерности массива: ");
+}
 string[] strArray = new string[size];  // создание массива заданного размера
 
+for (int i = 0; i < size; i++)   // Вводим элементы массива
+{
+   Console.Write($"Введите элемент {i + 1}: ");
+            string input = Console.ReadLine();
+            
+            while (string.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine("Пожалуйста, введите строку: ");
+                input = Console.ReadLine();
+            }
+
+            strArray[i] = input;
+}
+
+FilterAndPrintShortStrings(strArray); // Вызываем метод для фильтрации и вывода строк
+Console.ReadLine(); //удержание консоли
